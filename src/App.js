6d1a0a2804c6b5a1d3f0be9dc1950eb6ce5d3793
Ramdinpuia29 <Rdpahmar@gmail.com>
2021-07-01 12:14:22 +0530

    Routing, Favicon and resize

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
-      <Hero />
+      <Route path="/" exact component={Hero} />
       <Footer />
-    </Router>
+    </>
   );
 };
 
